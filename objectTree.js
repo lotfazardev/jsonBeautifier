a3ce0c95be2c42bf61b525f947a743a059b45ab9
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-03-31 00:05:49 +0430

    first release

---
+++ objectTree.js
@@ -0,0 +1,53 @@
+// ++++++++++++++++++++++++++++++++ sky is the limit ++++++++++++++++++++++++++++++++//
+// coded by Mohammad Hossien Lotfazar 
+// github link : https://github.com/lotfazardev/
+
+function objTree(obj){ //main func
+    const Obj = initObj(obj); 
+    visualisationObj(Obj); 
+}
+
+function initObj(arg){ //init func
+    if ( typeof arg === "object" ) {
+        return arg
+    }else{
+        try{
+            return JSON.parse(arg)
+        }catch(e){
+            console.log("invalid value")
+            return({})
+        }
+    }
+}
+function visualisationObj(Obj){ // visualisation body func
+    const marginTitle = "" ;
+    const marginItem = "\t"
+    visualisation(Obj, marginTitle, marginItem)
+}
+function visualisation(Obj, marginTitle, marginItem){ // //visualisation core func
+    for(let i in Obj){
+        if(typeof Obj[i] === "object" && Obj[i] != null){ //if current element is Object
+            if(Obj[i].__proto__.constructor.name == "Object"){ //if we sure it is an object
+                console.log(`${marginTitle} [-] ${i} :`)
+                visualisation(Obj[i], marginTitle + "\t", marginItem + "\t")
+            }else{ //so its itreable
+                for(let j in Obj[i]){
+                    if(Obj[i][j].__proto__.constructor.name == "Object"){
+                        visualisation(Obj[i], marginTitle + "\t", marginItem + "\t")
+                    }
+                }
+            }
+            continue
+        }
+        //data item extracting
+        let showValue = Obj[i] != null ? Obj[i] : "null"
+        //lets show it
+        console.log(`${marginTitle} [-] ${i} :`)
+        console.log(`${marginItem} ${showValue}`)
+    }
+
+}
+/* 
+    import it to your Nodejs app ussing this statement :
+    module.exports = objTree() ;
+*/
